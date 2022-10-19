8f17d884de2003b96ab309ce9a9b1d44618ea9a7
vinvui <vincent.vuillemin@gmail.com>
2022-10-19 19:15:41 +0200

    Hotfix #3

--- resources/views/livewire/trades-list.blade.php
+++ resources/views/livewire/trades-list.blade.php
@@ -15,23 +15,30 @@
         <div class="mx-auto max-w-7xl px-4 sm:px-8">
             <div class="flex flex-col justify-center gap-5">
                 @foreach($this->trades as $trade)
-                    <a href="{{ route('trade-store', ['trade_id' => $trade->id]) }}" class="flex justify-between">
-                        <div class="flex gap-3">
-                            @if($trade->type === "in")
-                                <svg class="text-emerald-600" width="45" height="45" viewBox="0 0 24 24"><path fill="currentColor" d="M22.001 12c0-5.523-4.477-10-10-10s-10 4.477-10 10s4.477 10 10 10s10-4.477 10-10Zm-14.53.28a.75.75 0 0 1-.073-.976l.072-.085l4.001-4a.75.75 0 0 1 .977-.073l.084.073l4 4.001a.75.75 0 0 1-.977 1.133l-.084-.072l-2.72-2.722v6.691a.75.75 0 0 1-.649.744l-.101.006a.75.75 0 0 1-.743-.648l-.007-.102V9.56l-2.72 2.72a.75.75 0 0 1-.977.073l-.084-.073Z"/></svg>
-                            @else
-                            <svg class="text-red-600 rotate-180" width="45" height="45" viewBox="0 0 24 24"><path fill="currentColor" d="M22.001 12c0-5.523-4.477-10-10-10s-10 4.477-10 10s4.477 10 10 10s10-4.477 10-10Zm-14.53.28a.75.75 0 0 1-.073-.976l.072-.085l4.001-4a.75.75 0 0 1 .977-.073l.084.073l4 4.001a.75.75 0 0 1-.977 1.133l-.084-.072l-2.72-2.722v6.691a.75.75 0 0 1-.649.744l-.101.006a.75.75 0 0 1-.743-.648l-.007-.102V9.56l-2.72 2.72a.75.75 0 0 1-.977.073l-.084-.073Z"/></svg>
-                            @endif
-                            <div>
-                                <p class="text-king-600 font-bold capitalize text-base">{{ $trade->name }}</p>
-                                <p class="text-king-600 font-medium text-sm">({{ $trade->date }})</p>
-                            </div>
+                <a href="{{ route('trade-store', ['trade_id' => $trade->id]) }}" class="flex justify-between">
+                    <div class="flex gap-3">
+                        @if($trade->type === "in")
+                        <svg class="text-emerald-600" width="45" height="45" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M22.001 12c0-5.523-4.477-10-10-10s-10 4.477-10 10s4.477 10 10 10s10-4.477 10-10Zm-14.53.28a.75.75 0 0 1-.073-.976l.072-.085l4.001-4a.75.75 0 0 1 .977-.073l.084.073l4 4.001a.75.75 0 0 1-.977 1.133l-.084-.072l-2.72-2.722v6.691a.75.75 0 0 1-.649.744l-.101.006a.75.75 0 0 1-.743-.648l-.007-.102V9.56l-2.72 2.72a.75.75 0 0 1-.977.073l-.084-.073Z" />
+                        </svg>
+                        @else
+                        <svg class="text-red-600 rotate-180" width="45" height="45" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M22.001 12c0-5.523-4.477-10-10-10s-10 4.477-10 10s4.477 10 10 10s10-4.477 10-10Zm-14.53.28a.75.75 0 0 1-.073-.976l.072-.085l4.001-4a.75.75 0 0 1 .977-.073l.084.073l4 4.001a.75.75 0 0 1-.977 1.133l-.084-.072l-2.72-2.722v6.691a.75.75 0 0 1-.649.744l-.101.006a.75.75 0 0 1-.743-.648l-.007-.102V9.56l-2.72 2.72a.75.75 0 0 1-.977.073l-.084-.073Z" />
+                        </svg>
+                        @endif
+                        <div>
+                            <p class="text-king-600 font-bold capitalize text-base">{{ $trade->name }}</p>
+                            <p class="text-king-600 font-medium text-sm">({{ $trade->date }})</p>
                         </div>
-                        <div class='flex flex-col items-end'>
-                            <p class="text-king-600 font-medium text-sm">{{ $trade->cost }}€</p>
-                            <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="1.41431" width="10" height="2" rx="1" transform="rotate(45 1.41431 0)" fill="#151850"/><rect y="13.071" width="10" height="2" rx="1" transform="rotate(-45 0 13.071)" fill="#151850"/></svg>
-                        </div>
-                    </a>
+                    </div>
+                    <div class='flex flex-col items-end'>
+                        <p class="text-king-600 font-medium text-sm">{{ $trade->cost }}€</p>
+                        <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect x="1.41431" width="10" height="2" rx="1" transform="rotate(45 1.41431 0)" fill="#151850" />
+                            <rect y="13.071" width="10" height="2" rx="1" transform="rotate(-45 0 13.071)" fill="#151850" />
+                        </svg>
+                    </div>
+                </a>
                 @endforeach
             </div>
         </div>
