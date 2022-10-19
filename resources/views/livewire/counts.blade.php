2114debc4c580af4f1267dcbca0c9167e7256be6
vinvui <vincent.vuillemin@gmail.com>
2022-10-19 18:58:29 +0200

    Hotfix #1

--- resources/views/livewire/counts.blade.php
+++ resources/views/livewire/counts.blade.php
@@ -3,8 +3,8 @@
         <div class="flex flex-col items-center">
             <h2 class="text-honey text-lg font-bold">Recette</h2>
             <div class="flex flex-col items-center">
-                <p class="text-emerald-600 font-medium">2500€</p>
-                <p class="text-honey/50 font-thin text-xs">pour ce mois</p>
+                <p class="text-red-600 font-medium"> -500€</p>
+                <p class="text-gray-300 font-thin text-xs -mt-2">mois en cours</p>
             </div>
         </div>
         <div class="flex flex-col items-center">
@@ -15,14 +15,14 @@
             <h2 class="text-honey text-lg font-bold">Epargne</h2>
             <div class="flex flex-col items-center">
                 <p class="text-emerald-600 font-medium">2500€</p>
-                <p class="text-honey/50 font-thin text-xs">pour ce mois</p>
+                <p class="text-gray-300 font-thin text-xs -mt-2">mois en cours</p>
             </div>
         </div>
     </div>
     <div class="flex justify-around">
         <a href="{{ route('trades-list') }}" class="{{ request()->routeIs('trades-list') ? 'border-king-600' : 'border-transparent' }} border-b-2 relative px-3 py-2 text-king-600">
             <svg width="32" height="32" viewBox="0 0 20 20">
-                <path fill="currentColor" stroke-width="0.5" stroke="#151850" d="M7.354 13.854L9 12.207a.5.5 0 1 0-.707-.707l-.793.793V6.5a.5.5 0 0 0-1 0v5.793l-.793-.793a.5.5 0 1 0-.707.707l1.646 1.647a.5.5 0 0 0 .708 0ZM15 7.793l-1.646-1.647a.5.5 0 0 0-.708 0L11 7.793a.5.5 0 1 0 .707.707l.793-.793V13.5a.5.5 0 1 0 1 0V7.707l.793.793A.5.5 0 1 0 15 7.793ZM18 10a8 8 0 1 1-16 0a8 8 0 0 1 16 0Zm-8 7a7 7 0 1 0 0-14a7 7 0 0 0 0 14Z"/>
+                <path fill="currentColor" stroke-width="0.5" stroke="currentColor" d="M7.354 13.854L9 12.207a.5.5 0 1 0-.707-.707l-.793.793V6.5a.5.5 0 0 0-1 0v5.793l-.793-.793a.5.5 0 1 0-.707.707l1.646 1.647a.5.5 0 0 0 .708 0ZM15 7.793l-1.646-1.647a.5.5 0 0 0-.708 0L11 7.793a.5.5 0 1 0 .707.707l.793-.793V13.5a.5.5 0 1 0 1 0V7.707l.793.793A.5.5 0 1 0 15 7.793ZM18 10a8 8 0 1 1-16 0a8 8 0 0 1 16 0Zm-8 7a7 7 0 1 0 0-14a7 7 0 0 0 0 14Z"/>
             </svg>
         </a>
         <a href="{{ route('trade-store', ['trade_id' => null]) }}" class="{{ Str::contains($this->url, 'trade/') || Str::endsWith($this->url, 'trade') ? 'border-king-600' : 'border-transparent' }} border-b-2 relative px-3 py-2 text-king-600">
