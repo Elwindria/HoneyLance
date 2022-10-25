1b6f3c0cb89d860477a1e2f54466d4823dfb4d9c
vinvui <vincent.vuillemin@gmail.com>
2022-10-25 10:56:37 +0200

    Fix UI auth

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,11 +1,9 @@
-<div class="h-screen flex flex-col justify-center bg-honey-light/40 gap-4">
-    <div class=""></div>
-    <div class="flex flex-col gap-12">
-        <div class="flex items-center justify-center">
+<div class="h-screen flex flex-col justify-between bg-honey-light/40">
+
+        <div class="flex h-full items-center justify-center">
             {{ $logo }}
         </div>
-        <div class="relative rounded-3xl bg-white">
+        <div class="relative rounded-t-3xl bg-white">
             {{ $slot }}
         </div>
-    </div>
 </div>
