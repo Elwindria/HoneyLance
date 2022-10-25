d142eed082cb42263344be674a28c1b49fb9f6a7
Elwindria <p.lopez2000@laposte.net>
2022-10-25 12:12:14 +0200

    légère modification css

--- resources/views/app/counts.blade.php
+++ resources/views/app/counts.blade.php
@@ -7,10 +7,10 @@
                 <p class="text-honey-dark/80 font-thin text-xs -mt-2">mois en cours</p>
             </div>
         </div>
-        <div class="flex flex-col items-center">
+        <a href="{{ route('user-settings') }}" class="flex flex-col items-center">
             <h2 class="text-king text-lg font-bold">Salaire</h2>
             <p class="{{ auth()->user()->setting->salary > 0 ? 'text-emerald-600' : 'text-red-600' }} font-medium">{{ number_format(auth()->user()->setting->salary, 0, ',', ' ') }} €</p>
-        </div>
+        </a>
         <div class="flex flex-col items-center">
             <h2 class="text-king text-lg font-bold">Epargne</h2>
             <div class="flex flex-col items-center">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,8 @@
                                 @csrf
                                 <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" class="flex items-center text-red-600">
                                     <svg width="32" height="32" class="h-4 w-4 mr-2" viewBox="0 0 24 24">
-                                        <path fill="currentColor" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2a9.985 9.985 0 0 1 8 4h-2.71a8 8 0 1 0 .001 12h2.71A9.985 9.985 0 0 1 12 22zm7-6v-3h-8v-2h8V8l5 4l-5 4z" /></svg>
+                                        <path fill="currentColor" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2a9.985 9.985 0 0 1 8 4h-2.71a8 8 0 1 0 .001 12h2.71A9.985 9.985 0 0 1 12 22zm7-6v-3h-8v-2h8V8l5 4l-5 4z" />
+                                    </svg>
                                     Me déconnecter
                                 </a>
                             </form>
