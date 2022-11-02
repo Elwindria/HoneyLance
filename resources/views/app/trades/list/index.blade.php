01681e1aabd4ef1fa1d20e84268b251588f82fc9
Elwindria <p.lopez2000@laposte.net>
2022-11-02 12:27:43 +0100

    component index TRade list + search

--- resources/views/app/trades/list/index.blade.php
+++ resources/views/app/trades/list/index.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout> --}}
     <div class="max-h-screen">
 
         @livewire('app.counts')
@@ -8,7 +8,7 @@
             <div class="flex flex-col gap-4 mt-2 pt-6 pb-2 mb-2 mx-auto max-w-7xl px-4 sm:px-8 sticky top-0 bg-white rounded-t-3xl z-40">
                 <div class="flex justify-center">
                     <div class="relative">
-                        <input class="block w-full appearance-none rounded-3xl pt-1 pb-2 border-honey border-2 h-8 bg-white focus:ring focus:ring-honey-light/50 focus:border-transparent text-king" placeholder="rechercher..." type="text" style="caret-color: rgb(107, 114, 128);" tabindex="0">
+                        <input wire:model="search" class="block w-full appearance-none rounded-3xl pt-1 pb-2 border-honey border-2 h-8 bg-white focus:ring focus:ring-honey-light/50 focus:border-transparent text-king" placeholder="rechercher..." type="text" style="caret-color: rgb(107, 114, 128);" tabindex="0">
                         <svg class="pointer-events-none absolute top-1 right-3 h-6 w-6 fill-slate-400" xmlns="http://www.w3.org/2000/svg">
                             <path d="M20.47 21.53a.75.75 0 1 0 1.06-1.06l-1.06 1.06Zm-9.97-4.28a6.75 6.75 0 0 1-6.75-6.75h-1.5a8.25 8.25 0 0 0 8.25 8.25v-1.5ZM3.75 10.5a6.75 6.75 0 0 1 6.75-6.75v-1.5a8.25 8.25 0 0 0-8.25 8.25h1.5Zm6.75-6.75a6.75 6.75 0 0 1 6.75 6.75h1.5a8.25 8.25 0 0 0-8.25-8.25v1.5Zm11.03 16.72-5.196-5.197-1.061 1.06 5.197 5.197 1.06-1.06Zm-4.28-9.97c0 1.864-.755 3.55-1.977 4.773l1.06 1.06A8.226 8.226 0 0 0 18.75 10.5h-1.5Zm-1.977 4.773A6.727 6.727 0 0 1 10.5 17.25v1.5a8.226 8.226 0 0 0 5.834-2.416l-1.061-1.061Z">
                             </path>
@@ -16,7 +16,7 @@
                     </div>
                 </div>
                 <div class="flex justify-between">
-                    <button wire:click="switchSummaryType('all')" class="text-king-light border-b border-king-light hover:text-honey px-3 py-2 font-medium text-sm">Tout ({{ App\Models\Trade::where('user_id', auth()->user()->id)->whereIn('type', ['in', 'out'])->count() }})</button>
+                    <button wire:click="switchSummaryType('all')" class="text-king-light border-b border-king-light hover:text-honey px-3 py-2 font-medium text-sm">Tout ({{ $this->search }})</button>
                     <button wire:click="switchSummaryType('in')" class="text-king/50 border-b border-white hover:text-honey px-3 py-2 font-medium text-sm">Entrée</button>
                     <button wire:click="switchSummaryType('out')" class="text-king/50 border-b border-white hover:text-honey px-3 py-2 font-medium text-sm">Sortie</button>
                     <button wire:click="switchSummaryType('fixed')" class="text-king/50 border-b border-white hover:text-honey px-3 py-2 font-medium text-sm whitespace-nowrap">Frais Fixe</button>
@@ -25,7 +25,7 @@
 
             <div class="mx-auto max-w-7xl px-4 sm:px-8 pb-12">
 
-                @livewire('app.trade-list', ['page' => 1, 'perPage' => 10, 'key' => 'trades-page-' . 1])
+                @livewire('app.trade-list', ['page' => 1, 'perPage' => 10, 'key' => 'trades-page-' . 1, 'search' => $this->search])
 
                 <button class="fixed bottom-2 right-2 shadow-lg bg-honey-light p-2 rounded-lg text-honey-dark" x-show="scrollBackTop" x-on:scroll.window="scrollBackTop = (window.pageYOffset > window.outerHeight * 0.5) ? true : false" @click.window="window.scrollTo({top: 0, behavior: 'smooth'})" x-transition.scale.origin.bottom aria-label="Back to top">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -37,4 +37,4 @@
         </div>
 
     </div>
-</x-app-layout>
+{{-- </x-app-layout> --}}
